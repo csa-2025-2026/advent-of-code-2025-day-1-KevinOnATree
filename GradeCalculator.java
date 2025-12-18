414ea757375eb070f64ba55b4c4bcc4448552688
KevinOnATree <kon100@student.hbuhsd.edu>
2025-12-18 22:54:36 +0000

    Finished Grade and Advent Pass

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -8,13 +8,17 @@ public class GradeCalculator
     {
         File file = new File("grades.txt");  // this is your problem input as a File object
         Scanner sc = new Scanner(file);   // the Scanner object is now reading from the file object
-
         // Write code that reads from the grades.txt file and calculates the average grade
-        while (sc.hasNext())  // as long as the Scanner has more lines to read from the file...
+        int sum = 0;
+        int amount = 0;
+        while (sc.hasNext())  // as long as the Scanner has more lines to read from the file..
         {
-
+            int grade = sc.nextInt();
+            sum += grade;
+            amount++;
         }
-
+            sum /= amount;
+        System.out.println(sum);
 
         sc.close();
     }
--- Main.java
+++ Main.java
@@ -6,31 +6,48 @@ public class Main
 {
   public static void main(String[] args) throws IOException
   {
-    File file = new File(input.txt);  // this is your problem input as a File object
+    File file = new File("input.txt");  // this is your problem input as a File object
     Scanner sc = new Scanner(file);   // the Scanner object is now reading from the file object
 
 
-    int answer = 0;
+    int answer = 50;
+    boolean isRight;
+    int count = 0;
+
     while (sc.hasNext())  // as long as the Scanner has more lines to read from the file...
     {
       String line = sc.nextLine();  // gets the next line of input.  This is like "R802"
-
       // TODO: Write code that gets the turning direction from the line.  This should be "L" or "R"
-
-
+        
       // TODO: Write code that gets a substring of the line that contains only the numbers
       // For R802, this would be a String variable with the value "802"
-
+      String num = line.substring(1, line.length());
 
       // TODO: Using the parseInt method from the Integer class, convert the String "802" into an int
+      int newnum = Integer.parseInt(num);
+
+      // TODO: Now that you have the turning direction, and the turning amount, see if the turn results in a 0 
 
 
-      // TODO: Now that you have the turning direction, and the turning amount, see if the turn results in a 0
+      if (line.substring(0,1).equals("R")) {
+          answer += newnum;
+        } else {
+          answer -= newnum;
+        }
 
+      answer = answer % 100;
 
+      if(answer < 0) {
+        answer = answer + 100;
+
+      }
+
+      if (answer == 0) {
+        count++;
+      
+      }
     }
-
-    System.out.println("The password is " + answer);
+      System.out.println("The real password is " + count);
     sc.close();
   }
 }
